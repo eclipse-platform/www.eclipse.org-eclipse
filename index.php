a37dcfa3b21f40b65ac8731a4681a3a5cdcc16c1
bbiggs <bbiggs>
2005-09-08 16:05:35 +0000

    Import pages from platform-swt-home.

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>SWT: The Standard Widget Toolkit</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+    <link rel="stylesheet" href="swt.css" type="text/css">
+    <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
+</head>
+<body bgcolor="#ffffff" text="#000000">
+<table width="875px" style="border-bottom: 1px solid #000000;">
+<colgroup><col width="125px"><col width="750px"></colgroup>
+<tr><?php include "sidebar.php"; ?>
+<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT: The Standard Widget Toolkit</h1>
+
+<p>SWT is an <a href="http://www.eclipse.org/legal/">open source</a> widget toolkit for Java designed to provide efficient,
+portable access to the user-interface facilities of the operating systems
+on which it is implemented.</p>
+
+<table width="100%">
+<tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Releases</font>
+</table>
+
+<table width="100%">
+<tr><td width="50%" valign="top">
+  <table width="100%">
+  <tr><td>
+      <b><font size="+1">Stable</font></b><br>
+      <a href="http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.1-200506271435/swt-3.1-win32-win32-x86.zip">Windows</a>,
+      <a href="http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.1-200506271435/swt-3.1-gtk-linux-x86.zip">Linux</a>,
+      <span style="white-space: nowrap;"><a href="http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.1-200506271435/swt-3.1-carbon-macosx-ppc.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.1-200506271435/index.php#swt" target="_top">more...</a><br>
+      <small>3.1.0 - <span style="white-space: nowrap;">27 June 2005</span></small><br>
+  <ul>
+  <li><a href="R3_1/plan.html">3.1 Maintenance Plan</a></li>
+  <li><a href="R3_1/new_and_noteworthy.html">New and Noteworthy</a></li>
+  <li><a href="language.html">Language Pack</a></li>
+  </ul>
+  </table>
+<td width="50%" valign="top">
+  <table width="100%">
+  <tr><td>
+      <b><font size="+1">Development</font></b><br>
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.2M1-200508111530/download.php?dropFile=swt-3.2M1-win32-win32-x86.zip">Windows</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.2M1-200508111530/download.php?dropFile=swt-3.2M1-gtk-linux-x86.zip">Linux</a>,
+      <span style="white-space: nowrap;"><a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.2M1-200508111530/download.php?dropFile=swt-3.2M1-carbon-macosx-ppc.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.2M1-200508111530/index.php#swt" target="_top">more...</a><br>
+      <small>3.2 M1 - <span style="white-space: nowrap;">11 August 2005</span></small><br>
+  <ul>
+  <li><a href="R3_2/plan.html">3.2 Development Plan</a></li>
+  <li><a href="R3_2/new_and_noteworthy.html">New and Noteworthy</a></li>
+  </ul>
+  </table>
+</table>
+
+<table width="100%">
+<tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Resources</font>
+</table>
+
+<ul>
+<li><a href="eclipse.php">How to develop SWT applications in Eclipse</a></li>
+<li><a href="widgets.php">Widget screenshots</a> with links to documentation</li>
+<li><a href="snippets.php">Snippets</a>, helpful code examples</li>
+<li><a href="http://help.eclipse.org/help31/index.jsp?topic=/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/package-summary.html" target="_top">Online Javadoc</a></li>
+<li><a href="http://eclipse.org/articles/main.html#SWT">Articles about SWT</a> from eclipse.org</li>
+<li><a href="cvs.php">How to use SWT from CVS</a></li>
+<li><a href="tools.php">Tools and Plug-ins</a> for SWT developers</li>
+<li><a href="faq.php">The SWT FAQ</a> answers many common questions</li>
+<li><a href="docs.php">Books and additional documentation</a></li>
+<li><a href="community.php">SWT Community Page</a></li>
+</ul>
+
+<table width="100%">
+<tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Contact Us</font>
+</table>
+
+<p>The <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>
+is a very active user discussion and help forum.
+See the
+<a href="http://www.eclipse.org/newsgroups/index.html">Eclipse
+newsgroups page</a>
+[<a href="http://dev.eclipse.org/newslists/news.eclipse.platform.swt/maillist.html">archive</a>,
+<a href="http://www.eclipse.org/search/search.cgi">search</a>].</p>
+
+<p>SWT development is discussed and tracked in the
+<a href="http://bugs.eclipse.org/bugs/" target="_top">Eclipse bugzilla</a> under the
+Platform/SWT component.  You can subscribe to the new bug inbox by
+watching <i>platform-swt-inbox@eclipse.org</i> from your
+bugzilla email preferences.</p>
+
+<p>If you are modifying or porting SWT and have questions, try the
+<a href="http://dev.eclipse.org/mailman/listinfo/platform-swt-dev">SWT developer mailing list</a>
+[<a href="http://dev.eclipse.org/mhonarc/lists/platform-swt-dev/maillist.html">archive</a>,
+<a href="http://www.eclipse.org/search/search.cgi">search</a>].</p>
+
+<table width="100%">
+<tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Older Releases</font>
+</table>
+
+<ul>
+<li><a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.0.2-200503110845/index.php">SWT 3.0.2</a>, 11 March 2005.
+    <ul>
+    <li><a href="R3_0/plan.html">3.0 Development Plan</a></li>
+    <li><a href="R3_0/new_and_noteworthy.html">New and Noteworthy</a> items
+        for each milestone</li>
+    </ul>
+</ul>
+<ul>
+<li><a href="http://download.eclipse.org/downloads/drops/R-2.1.3-200403101828/index.php">SWT 2.1.3</a>, 10 March 2004.
+    <ul>
+    <li><a href="R2_1/plan.html">2.1 Development Plan</a></li>
+    </ul>
+</ul>
+
+<td valign="top" width="240px">
+<center>
+<p><img src="images/win-example.png" alt="Windows" width="208" height="196"></p>
+<p><img src="images/lin-example.png" alt="Linux" width="212" height="224"></p>
+<p><img src="images/mac-example.png" alt="Mac OS X" width="232" height="237"></p>
+<p><img src="images/mot-example.png" alt="Motif" width="232" height="210"></p>
+<p><img src="images/pho-example.png" alt="Photon" width="234" height="240"></p>
+</center>
+</table>
+</body>
+</html>
