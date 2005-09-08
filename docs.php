10de328e4712238803f96235318417ddd2ba108d
bbiggs <bbiggs>
2005-09-08 15:15:47 +0000

    Add the sidebar to the docs page.

--- docs.php
+++ docs.php
@@ -7,8 +7,10 @@
     <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
 </head>
 <body bgcolor="#ffffff" text="#000000">
-<table width="510px" style="border-bottom: 1px solid #000000;">
-<tr><td><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT Resources and Links</h1>
+<table width="635px" style="border-bottom: 1px solid #000000;">
+<colgroup><col width="125px"><col width="510px"></colgroup>
+<tr><?php include "sidebar.php"; ?>
+<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT Resources and Links</h1>
 
 <p>This page contains links and references to books, tutorials, and
 articles about SWT.</p>
