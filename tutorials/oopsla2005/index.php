d78c172b60530c518f4e597775c7ae827f3bb7c2
veronika <veronika>
2005-10-13 18:30:30 +0000

    *** empty log message ***

--- tutorials/oopsla2005/index.php
+++ tutorials/oopsla2005/index.php
@@ -8,8 +8,12 @@
     <link rel="stylesheet" href="http://www.eclipse.org/swt/swt.css" type="text/css">
     <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
 </head>
-<body>
+<body bgcolor="#ffffff" text="#000000">
+<table width="875px" class="swtpage">
+<colgroup><col width="125px"><col width="750px"></colgroup>
+<tr><?php include "../sidebar.php"; ?>
+<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">Extending the Standard Widget Toolkit</h1>
 <p>Download the <a href="t22-irvine.ppt">powerpoint slides</a> from 
-OOPSLA 2005 tutorial "Extending the Standard Widget Toolkit"</p>
+OOPSLA 2005 Tutorial "Extending the Standard Widget Toolkit"</p>
 </body>
 </html>
