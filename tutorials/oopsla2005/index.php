fe50c87c4c3c2d7b2555192bfed8e754375ed850
veronika <veronika>
2005-10-13 18:29:02 +0000

    *** empty log message ***

---
+++ tutorials/oopsla2005/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Extending the Standard Widget Toolkit</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+    <link rel="stylesheet" href="http://www.eclipse.org/swt/swt.css" type="text/css">
+    <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
+</head>
+<body>
+<p>Download the <a href="t22-irvine.ppt">powerpoint slides</a> from 
+OOPSLA 2005 tutorial "Extending the Standard Widget Toolkit"</p>
+</body>
+</html>
