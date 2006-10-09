de5545c27773482ed9e15e7ea3c236fb70dfd9aa
steve <steve>
2006-10-09 20:15:06 +0000

    *** empty log message ***

--- jws/index.php
+++ jws/index.php
@@ -12,7 +12,7 @@
 <table width="800px" class="swtpage">
 <colgroup><col width="125px"><col width="675px"></colgroup>
 <tr><?php include "../sidebar.php"; ?>
-<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">How to deploy SWT Applications with Java Web Start</h1>
+<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">How to deploy SWT Applications using Java Web Start</h1>
 
 <p>SWT applications can be deployed using Java Web Start (JWS).  Here are the steps to help you package and deploy your SWT application with JWS.</p>
 <p>First, create jar archives with SWT and its libraries for each platform that you wish to run on: (see <a href="#platformsnote">supported platforms</a> note)</p>
