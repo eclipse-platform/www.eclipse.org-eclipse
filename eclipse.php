44c01d0d983c529343ea44b48dc73bb607b33d46
carolyn <carolyn>
2006-10-12 16:39:52 +0000

    updated for eclipse 3.2, and added run as->java app

--- eclipse.php
+++ eclipse.php
@@ -23,7 +23,9 @@ download.  This document will help you get set up.</p>
 
 <p>The SWT .zip file can then be imported into your workspace. In the
 <i>File</i> menu, choose <i>Import</i> and select the
-<i>Existing Projects Into Workspace</i> wizard.</p>
+<i>Existing Projects Into Workspace</i> wizard.
+(In newer versions of eclipse, you can find <i>Existing Projects Into Workspace</i>
+in the <i>General</i> category).</p>
 
 <center><p><img src="images/existing.png" alt="Existing Projects Into Workspace Wizard"></p></center>
 
@@ -44,7 +46,8 @@ Eclipse features such as the Javadoc view and code assist.</p>
 
 <center><p><img src="images/swt-example.png" alt="SWT Eclipse Example"></p></center>
 
-<p>And there you go!</p>
+<p>Now you can run any main class in your project by selecting the class and then selecting
+<i>Run &gt; Run As &gt; Java Application</i></p>
 
 </table>
 </body>
