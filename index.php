bcc6eb3887651b5981ab7c5d6983037a6a69e286
steve <steve>
2006-12-20 20:02:21 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ on which it is implemented.</p>
 <li><a href="eclipse.php">How to develop SWT applications in Eclipse</a></li>
 <li><a href="examples.php">How to run the ControlExample and other SWT examples</a></li>
 <li><a href="macosx/">How to deploy SWT applications on Mac OS X</a></li>
-<li><a href="jws/">How to deploy SWT Applications using Java Web Start</a></li>
+<li><a href="jws/">How to deploy SWT applications using Java Web Start</a></li>
 <li><a href="cvs.php">How to use SWT from CVS</a></li>
 <li><a href="jnigen.php">How to generate the SWT JNI Code</a></li>
 <li><a href="opengl/">Using OpenGL with SWT 3.2</a></li>
