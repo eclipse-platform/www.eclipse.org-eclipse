0fddd8f8bd9c32d907757e32d075c62ac16d01d5
steve <steve>
2007-10-03 20:20:19 +0000

    *** empty log message ***

--- faq.php
+++ faq.php
@@ -304,7 +304,9 @@ the <a href="http://dev.eclipse.org/mailman/listinfo/platform-swt-dev">SWT devel
       <li>Pango 1.0.3</li>
       <li>Freetype 2.1.2</li>
     </ul>
+    <p>
     You can determine which version of GTK you are running using <pre>rpm -q gtk2</pre>.
+    </p>
   </dd>
 
   <dt><strong><a name="carbonapp">Q: On carbon, how do I run an SWT application from the command line?</a></strong></dt>
