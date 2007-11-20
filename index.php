1acef54082184e9280534cb2c538de120ae24f20
carolyn <carolyn>
2007-11-20 17:35:30 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -18,6 +18,33 @@ portable access to the user-interface facilities of the operating systems
 on which it is implemented.</p>
 
 <table width="100%">
+<tr><th bgcolor="#008f60" align="left" style="padding: 2px;"><font color="#ffffff">News - November, 2007</font>
+</table>
+
+<p>We are looking for help with the cocoa port of SWT.
+See Steve's blog for background discussion:
+<ul>
+<li><a href="http://inside-swt.blogspot.com/2007/11/hacking-at-apple.html">Hacking at Apple</a></li>
+<li><a href="http://inside-swt.blogspot.com/2007/11/great-success.html">Great Success</a></li>
+<li><a href="http://inside-swt.blogspot.com/2007/11/ok-community-blow-your-brains-out.html">Call For Volunteers</a></li>
+</ul>
+</p><p>To get started, <a href="http://www.eclipse.org/swt/cvs.php">load SWT from CVS</a> into your eclipse workspace.
+The projects you need are:
+<ul>
+<li>org.eclipse.swt</li>
+<li>org.eclipse.swt.cocoa.macosx</li>
+<li>org.eclipse.swt.examples (not required, but useful for testing)</li>
+</ul>
+<p>If you find a bug, <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Platform">open an eclipse bug report</a>
+with <b>Component: SWT</b>, <b>Platform: Macintosh</b> and <b>OS: Mac OSX - Cocoa</b>.
+</p>
+<p>Then see if you can figure out how to fix the problem, and if so, attach a patch to the bug report.
+Discussion can take place on the <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>.
+Thanks to those who are already jumping in and getting their feet wet!
+</p><p>- The SWT Team
+</p>
+
+<table width="100%">
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Releases</font>
 </table>
 
