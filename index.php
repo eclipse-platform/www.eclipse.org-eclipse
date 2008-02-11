41bddaf0dc8558f75f2c31c33d008768e6a62b1a
gheorghe <gheorghe>
2008-02-11 15:52:10 +0000

    Fixed News section

--- index.php
+++ index.php
@@ -18,33 +18,6 @@ portable access to the user-interface facilities of the operating systems
 on which it is implemented.</p>
 
 <table width="100%">
-<tr><th bgcolor="#008f60" align="left" style="padding: 2px;"><font color="#ffffff">News - November, 2007</font>
-</table>
-
-<p>We are looking for help with the cocoa port of SWT.
-To get started, <a href="http://www.eclipse.org/swt/cvs.php">load SWT from CVS</a> into your eclipse workspace.
-The projects you need are:
-<ul>
-<li>org.eclipse.swt</li>
-<li>org.eclipse.swt.cocoa.macosx</li>
-<li>org.eclipse.swt.examples (not required, but useful for testing)</li>
-</ul>
-<p>If you find a bug, <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Platform&component=SWT">open an eclipse bug report</a>
-with <b>Platform: Macintosh</b> and <b>OS: Mac OSX - Cocoa</b>.
-Also, you can <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=Eclipse&product=Platform&component=SWT&op_sys=Mac+OS+X+-+Cocoa&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">
-pick an interesting bug from here</a>.
-Then see if you can figure out how to fix the problem, and if so, attach a patch to the bug report.
-Discussion can take place on the <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>.
-</p><p>See Steve Northover's blog for a bit of background:
-<ul>
-<li><a href="http://inside-swt.blogspot.com/2007/11/hacking-at-apple.html">Hacking at Apple</a></li>
-<li><a href="http://inside-swt.blogspot.com/2007/11/great-success.html">Great Success</a></li>
-<li><a href="http://inside-swt.blogspot.com/2007/11/ok-community-blow-your-brains-out.html">Call For Volunteers</a></li>
-</ul>
-</p><p>Thanks to those who are already jumping in and getting their feet wet!
-</p>
-
-<table width="100%">
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Releases</font>
 </table>
 
@@ -167,5 +140,32 @@ bugzilla email preferences.</p>
 <p><img src="images/pho-example.png" alt="Photon" width="225" height="227"></p>
 </center>
 </table>
+
+<table width="100%">
+<tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Cocoa Port</font>
+</table>
+
+<p>We are looking for help with the cocoa port of SWT.
+To get started, <a href="http://www.eclipse.org/swt/cvs.php">load SWT from CVS</a> into your eclipse workspace.
+The projects you need are:
+<ul>
+<li>org.eclipse.swt</li>
+<li>org.eclipse.swt.cocoa.macosx</li>
+<li>org.eclipse.swt.examples (not required, but useful for testing)</li>
+</ul>
+<p>If you find a bug, <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Platform&component=SWT">open an eclipse bug report</a>
+with <b>Platform: Macintosh</b> and <b>OS: Mac OSX - Cocoa</b>.
+Also, you can <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=Eclipse&product=Platform&component=SWT&op_sys=Mac+OS+X+-+Cocoa&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">
+pick an interesting bug from here</a>.
+Then see if you can figure out how to fix the problem, and if so, attach a patch to the bug report.
+Discussion can take place on the <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>.
+</p><p>See Steve Northover's blog for a bit of background:
+<ul>
+<li><a href="http://inside-swt.blogspot.com/2007/11/hacking-at-apple.html">Hacking at Apple</a></li>
+<li><a href="http://inside-swt.blogspot.com/2007/11/great-success.html">Great Success</a></li>
+<li><a href="http://inside-swt.blogspot.com/2007/11/ok-community-blow-your-brains-out.html">Call For Volunteers</a></li>
+</ul>
+</p><p>Thanks to those who are already jumping in and getting their feet wet!
+</p>
 </body>
 </html>
