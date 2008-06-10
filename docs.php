1e8a7a54d0eca2e4c4edf313db9174f6671db27b
carolyn <carolyn>
2008-06-10 19:05:12 +0000

    add a few more books

--- docs.php
+++ docs.php
@@ -24,6 +24,8 @@
 <ul>
    <li><a href="http://www.amazon.com/exec/obidos/tg/detail/-/0321256638/qid=1088631104/sr=8-3/ref=sr_8_xs_ap_i3_xgl14/104-7716812-0314361?v=glance&amp;s=books&amp;n=507846">
        <em>"SWT: The Standard Widget Toolkit, Volume 1"</em></a> by Steve Northover and Mike Wilson</li>
+       <br>This book can be read online at the
+       <a href="http://safari.informit.com/9780321256638">publisher's web site</a>.
    <li><a href="http://www.amazon.com/exec/obidos/tg/detail/-/1590593251/qid=1088631104/sr=8-1/ref=pd_ka_1/104-7716812-0314361?v=glance&amp;s=books&amp;n=507846">
        <em>"The Definitive Guide to SWT and JFace"</em></a> by Rob Warner and Robert Harris</li>
    <li><a href="http://www.amazon.com/exec/obidos/ASIN/0321305027">
@@ -32,6 +34,15 @@
        <em>"Contributing to Eclipse: Principles, Patterns, and Plugins"</em></a> by Erich Gamma and Kent Beck (Chapter 34)</li>
    <li><a href="http://www.amazon.com/exec/obidos/tg/detail/-/1930110960/qid=1075410470//ref=sr_8_xs_ap_i1_xgl14/002-9415319-6500833?v=glance&amp;s=books&amp;n=507846">
        <em>"Eclipse in Action: A Guide for the Java Developer"</em></a> by David Gallardo, Ed Burnette, and Robert McGovern (Appendix D)</li>
+   <li><a href="http://www.amazon.com/Eclipse-Rich-Client-Platform-Applications/dp/0321334612/ref=pd_sim_b_title_2">
+       <em>"Eclipse Rich Client Platform: Designing, Coding, and Packaging Java(TM) Applications"</em></a> by Jeff McAffer and Jean-Michel Lemieux</li>
+   <li><a href="http://www.amazon.com/Eclipse-Building-Commercial-Quality-Plug-ins-2nd/dp/032142672X/ref=pd_sim_b_title_3">
+       <em>"Eclipse: Building Commercial-Quality Plug-ins"</em></a> by Eric Clayberg and Dan Rubel</li>
+   <li><a href="http://www.amazon.com/SWT-JFace-Action-Design-Eclipse/dp/1932394273/ref=pd_sim_b_title_4">
+       <em>"SWT/JFace in Action: GUI Design with Eclipse 3.0"</em></a> by Matthew Scarpino, Stephen Holder, Stanford Ng and Laurent Mihalkovic</li>
+   <li><a href="http://safari.informit.com/0596006411">
+       <em>"Eclipe"</em></a> and <a href="http://safari.informit.com/0596007108">
+       <em>"Eclipse Cookbook"</em></a> by Steve Holzner</li>
 </ul>
 
 <table width="100%">
