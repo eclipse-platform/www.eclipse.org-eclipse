1912e31d79d085e341d7671d1a6a4e83edb3c6d8
carolyn <carolyn>
2008-09-10 16:56:35 +0000

    update

--- cocoaport.php
+++ cocoaport.php
@@ -18,26 +18,30 @@
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Cocoa Port</font>
 </table>
 
-<p>We are looking for help with the cocoa port of SWT.
+<p>The Cocoa port of SWT is well underway. ControlExample and most of the SWT examples run, and
+we expect to have an Eclipse build of macosx.cocoa.x86 available for 3.4 M2.
+There are still areas that need work. Here is the port's status as of
+<a href="http://dev.eclipse.org/mhonarc/lists/platform-swt-dev/msg06076.html">August 27, 2008</a>.
+<p>If you want to help, here is what you can do.
 To get started, <a href="http://www.eclipse.org/swt/cvs.php">load SWT from CVS</a> into your eclipse workspace.
 The projects you need are:
 <ul>
 <li>org.eclipse.swt</li>
 <li>org.eclipse.swt.cocoa.macosx</li>
 <li>org.eclipse.swt.examples (not required, but useful for testing)</li>
-<li>org.eclipse.swt.tools (not required, but useful for adding extra <a href="http://www.eclipse.org/swt/macgen.php">platform interface</a>)</li>
+<li>org.eclipse.swt.tools (not required, unless you need to extend the <a href="http://www.eclipse.org/swt/macgen.php">Platform Interface</a>)</li>
 </ul>
-<p>If you find a bug, <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Platform&component=SWT&rep_platform=Macintosh&op_sys=Mac+OS+X+-+Cocoa">open an eclipse bug report</a>.
+</p><p>If you find a bug, <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Platform&component=SWT&rep_platform=Macintosh&op_sys=Mac+OS+X+-+Cocoa">open an eclipse bug report</a>.
 Also, you can <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=Eclipse&product=Platform&component=SWT&op_sys=Mac+OS+X+-+Cocoa&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">
 pick an interesting bug from here</a>, see if you can figure out how to fix the problem, and if so, attach a patch to the bug report.
-Discussion can take place on the <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>.
-</p><p>See Steve Northover's blog for a bit of background:
+Subscribe to the <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a> for discussion and to keep track of current developments.
+</p><p>For a history of the beginnings of the Cocoa port, see Steve Northover's blog:
 <ul>
 <li><a href="http://inside-swt.blogspot.com/2007/11/hacking-at-apple.html">Hacking at Apple</a></li>
 <li><a href="http://inside-swt.blogspot.com/2007/11/great-success.html">Great Success</a></li>
 <li><a href="http://inside-swt.blogspot.com/2007/11/ok-community-blow-your-brains-out.html">Call For Volunteers</a></li>
 </ul>
-</p><p>Thanks to those who are already jumping in and getting their feet wet!
+</p><p>Thanks to those who have helped so far!
 </p>
 
 </table>
