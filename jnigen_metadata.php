72de8db6dc4633d609134a6088767664d277e4ea
carolyn <carolyn>
2009-06-18 14:58:24 +0000

    minor changes, like typos and italics

--- jnigen_metadata.php
+++ jnigen_metadata.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <title>SWT JNI Tool Meta Data</title>
+    <title>SWT JNIGen Tool Metadata</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="Content-Style-Type" content="text/css">
     <link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
@@ -18,8 +18,8 @@
 application included in the SWT Tools bundle and available on the 
 <a href="updatesite.php">SWT Tools Update Sites</a>.
 
-This page describes the meta data used to annotate the native methods and
-structures to help the tool generate the appropriate C code. This meta data
+This page describes the metadata used to annotate the native methods and
+structures to help the tool generate the appropriate C code. This metadata
 is provide in the form of tags in the java doc comment. </p>
 
 
@@ -28,7 +28,7 @@ is provide in the form of tags in the java doc comment. </p>
 <ol>
 <li> <b>@jniclass</b> <tt>&lt;metadata&gt;</tt></li>
 
-	<p>Annotates a class. Classes can be either a structure class or a main class which contains natives.</p> 
+	<p>Annotates a class. Classes can be either a structure class or a main class that contains natives.</p> 
 
 <li> <b>@method</b> <tt>&lt;metadata&gt;</tt> </li>
 
@@ -45,7 +45,7 @@ is provide in the form of tags in the java doc comment. </p>
 
 <h3> Metadata </h3>
 
-<p> The meta data is a comma separated list of attributes of the form <i>key=value</i>. For example:
+<p> The metadata is a comma separated list of attributes of the form <i>key=value</i>. For example:
 
  <p><b>cast=(HWND),flags=flag1 flag2,accessor=othername</b></p> 
  
@@ -59,7 +59,7 @@ The key or value may not contain the comma character &lt;<b>,</b>&gt;. </p>
 <li> <b>accessor</b></li>
 	<p>Provide the C name/identifier to be used instead of the java name. This can be used by structure fields <i>@field</i> or native methods <i>@method</i>.</p>
 <li> <b>flags</b></li>
-	<p>Provide switches to control how the C code is generated. All tags may have this attribute. 
+	<p>Provide switches to control how the C code is generated. Any tag may have this attribute. 
 	Multiple flags are separated by a space character. See below for a list of the known flags.</p>
 </ol>
 
@@ -69,32 +69,32 @@ The key or value may not contain the comma character &lt;<b>,</b>&gt;. </p>
 
 <ol>
 <li> <b>no_gen</b></li>
-<p>Indicate that the item should not be generated. Used in: @jniclass @method @field</p>
+<p>Indicate that the item should not be generated. For example, custom natives are coded separately. Used in: <i>@jniclass</i>, <i>@method</i>, <i>@field</i></p>
 <li> <b>no_in</b></li>
-<p>Indidate that a native method parameter is an out only variable. This only makes sense if the parameter is a structure or an array of primitives. It is an optimization to avoid copying the java memory to C memory in the way in. Used in: @param</p>
+<p>Indicate that a native method parameter is an out only variable. This only makes sense if the parameter is a structure or an array of primitives. It is an optimization to avoid copying the java memory to C memory on the way in. Used in: <i>@param</i></p>
 <li> <b>no_out</b></li>
-<p>Indidate that a native method parameter is an in only variable. This only makes sense if the parameter is a structure or an array of primitives. It is an optimization to avoid copying the C memory from java memory in the way out. Used in: @param</p>
+<p>Indicate that a native method parameter is an in only variable. This only makes sense if the parameter is a structure or an array of primitives. It is an optimization to avoid copying the C memory from java memory on the way out. Used in: <i>@param</i></p>
 <li> <b>critical</b></li>
-<p>Indicate that <tt>GetPrimitiveArrayCritical()</tt> should be used instead of <tt>Get&lt;PrimitiveType&gt;ArrayElements()</tt> when transfering array of primitives from/to C. This is an optimization to avoid copying memory and must be used carefully. It is ok to be used in <tt>MoveMemory()</tt> and <tt>memmove()</tt> natives. Used in: @param<p>
+<p>Indicate that <tt>GetPrimitiveArrayCritical()</tt> should be used instead of <tt>Get&lt;PrimitiveType&gt;ArrayElements()</tt> when transfering array of primitives from/to C. This is an optimization to avoid copying memory and must be used carefully. It is ok to be used in <tt>MoveMemory()</tt> and <tt>memmove()</tt> natives. Used in: <i>@param</i><p>
 <li> <b>dynamic</b></li>
 <p>Indicate that a native method should be looked up dynamically. It is useful when having a dependence on a given library is not desirable. 
-The library name is specified in the *_custom.h file. Used in: @method<p>
+The library name is specified in the *_custom.h file. Used in: <i>@method</i><p>
 <li> <b>init</b></li>
-<p>Indicate that the associated C local variable for a native method parameter should be initialized with zeros. Used in: @param</p>
+<p>Indicate that the associated C local variable for a native method parameter should be initialized with zeros. Used in: <i>@param</i></p>
 <li> <b>struct</b></li>
-<p>Indicate that a structure parameter should be passed by value instead of by reference. This dereferences the parameter by prepending *. The parameter must not be NULL. Used in: @param</p>
+<p>Indicate that a structure parameter should be passed by value instead of by reference. This dereferences the parameter by prepending *. The parameter must not be NULL. Used in: <i>@param</i></p>
 <li> <b>unicode</b></li>
-<p>Indicates that <tt>GetStringChars()</tt>should be used instead of <tt>GetStringUTFChars()</tt> to get the characters of a java.lang.String passed as a parameter to native methods. Used in: @param</p>
+<p>Indicates that <tt>GetStringChars()</tt>should be used instead of <tt>GetStringUTFChars()</tt> to get the characters of a java.lang.String passed as a parameter to native methods. Used in: <i>@param</i></p>
 <li> <b>sentinel</b></li>
-<p>Indicate that the parameter of a native method is the sentinel (last parameter of a variable argument C function). The generated code is always the literal <tt>NULL</tt>. Some compilers expect the sentinel to be the literal <tt>NULL</tt> and output a warning if otherwise. Used in: @param</p>
+<p>Indicate that the parameter of a native method is the sentinel (last parameter of a variable argument C function). The generated code is always the literal <tt>NULL</tt>. Some compilers expect the sentinel to be the literal <tt>NULL</tt> and output a warning if otherwise. Used in: <i>@param</i></p>
 <li> <b>const</b></li>
-<p>Indicate that the native method represents a constant or global variable instead of a function. This omits <tt>()</tt> from the generated code. Used in: @method</p>
+<p>Indicate that the native method represents a constant or global variable instead of a function. This omits <tt>()</tt> from the generated code. Used in: <i>@method</i></p>
 <li> <b>cast</b></li>
-<p>Indicate that the C function should be casted to a prototype generated from the parameters of the native method. Useful for variable argument C functions. Used in: @method</p>
+<p>Indicate that the C function should be casted to a prototype generated from the parameters of the native method. Useful for variable argument C functions. Used in: <i>@method</i></p>
 <li> <b>jni</b></li>
-<p>Indicate that the native is part of the Java Native Interface. For exemple: <tt>NewGlobalRef()</tt>. Used in: @method</p>
+<p>Indicate that the native is part of the Java Native Interface. For exemple: <tt>NewGlobalRef()</tt>. Used in: <i>@method</i></p>
 <li> <b>address</b></li>
-<p>Indicate that the native method represents a structure global variable and the address of it should be returned to Java. This is done by prepending &amp;. Used in: @method</p>
+<p>Indicate that the native method represents a structure global variable and the address of it should be returned to Java. This is done by prepending &amp;. Used in: <i>@method</i></p>
 <li> <b>no_wince</b></li>
 <p>Indicate that the item should be #ifdef out in the Windows CE platform, but not in the regular win32 platform. </p>
 <li> <b>cpp</b></li>
