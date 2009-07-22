c96fbab85753eeb13560cf2ef8f77e4810a71837
gheorghe <gheorghe>
2009-07-22 17:35:38 +0000

    *** empty log message ***

---
+++ triage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Generating the SWT JNI Code</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+    <link rel="stylesheet" href="swt.css" type="text/css">
+    <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
+</head>
+<body bgcolor="#ffffff" text="#000000">
+<table width="875px" class="swtpage">
+<colgroup><col width="125px"><col width="750px"></colgroup>
+<tr><?php include "sidebar.php"; ?>
+<td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT Bug Triage</h1>
+<table id="swtComponents" border="1">
+	<tbody>
+		<tr>
+			<td>
+			<h3>Component</h3>
+			</td>
+			<td>
+			<h3>Description</h3>
+			</td>
+			<td>
+			<h3>Windowing System</h3>
+			</td>
+			<td>
+			<h3>Owner</h3>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</table>
+</body>
+</html>
