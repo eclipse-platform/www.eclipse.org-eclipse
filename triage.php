fc4968315138faab7bbf043c6de081daa8fb5554
gheorghe <gheorghe>
2009-07-24 20:04:57 +0000

    *** empty log message ***

--- triage.php
+++ triage.php
@@ -13,10 +13,62 @@
 <colgroup><col width="125px"><col width="750px"></colgroup>
 <tr><?php include "sidebar.php"; ?>
 <td valign="top" style="padding: 10px"><h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT Bug Triage</h1>
-
 <script type="text/javascript">
 window.onload = function() {
-	alert("hi!");
+	if (window.XMLHttpRequest)
+  	{
+ 		 //IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else if (window.ActiveXObject)
+  	{
+  		// IE6, IE5
+  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	else
+  	{
+  		alert("Your browser does not support XMLHTTP!");
+  	}
+   xmlhttp.onreadystatechange=function() {
+		if(xmlhttp.readyState==4){
+			//if (xmlhttp.status == 200) {
+				buildTable(xmlhttp.responseText);
+			//}else{
+			//	alert(xmlhttp.status);
+			//}
+		}
+	}
+	xmlhttp.open("GET","swt_triage.json",true);
+	xmlhttp.send(null);
+}
+
+function buildTable(loadedJSON){
+ var container = eval('(' + loadedJSON + ')');
+ var components = container.components;
+ var users = container.users;
+
+ table = document.getElementById('swtComponents');
+ var row = table.insertRow(0);
+ var component = row.insertCell(0);
+ var description = row.insertCell(1);
+ var owner = row.insertCell(2);
+ component.innerHTML = '<h3>Component</h3>';
+ description.innerHTML = "<h3>Description</h3>";
+ owner.innerHTML = "<h3>Owner</h3>";
+
+ for (i = 0; i < components.length; i++){
+	 row = table.insertRow(i+1);
+	 component = row.insertCell(0);
+	 description = row.insertCell(1);
+	 owner = row.insertCell(2);
+	 component.innerHTML = components[i].component;//'<div id="component">[<a href="javascript:viewBugsWithSummary(\'[' + components[i].component + ']\')">' + components[i].component + '</a>]</div>';
+	 description.innerHTML = components[i].description;
+	 var osList = "";
+	 for (j = 0; j < components[i].OS.length; j++) {
+	 	osList += '<li>' + components[i].OS[j].name + ' - ' + components[i].OS[j].owner + ','+ components[i].OS[j].second + '</li>'; 
+	 }
+	 owner.innerHTML = '<div id="owner"><ul>' + osList + '</ul></div>';
+ }
 }
 </script>
 <h2>SWT Bug Triage Procedure</h2>
@@ -41,514 +93,7 @@ triage process earlier in the year and we have decided to adopt their process.</
 </p> 
 <h2>SWT Component Areas</h2>
 <table id="swtComponents" border="1">
-	<tbody>
-		<tr>
-			<th>
-				<h3>Component</h3>
-			</th>
-			<th>
-				<h3>Description</h3>
-			</th>
-			<th>
-				<h3>Owner</h3>
-			</th>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[PI]')">Platform Interface (PI)</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - SSQ, FH</li>
-						<li>Carbon - SSQ, FH</li>
-						<li>Cocoa - SSQ, FH</li>
-						<li>Motif - SSQ, FH</li>
-						<li>Photon - RD, SSQ, FH</li>
-						<li>Win32 - SSQ, FH</li>
-						<li>WPF - SSQ, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Graphics]')">Graphics</a>
-				]
-				</div>
-			</td>
-			<td>
-				All graphics issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - SSQ, BG, FH</li>
-						<li>Carbon - SSQ, FH</li>
-						<li>Cocoa - KB, SSQ, FH</li>
-						<li>Motif - SSQ, FH</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - SSQ, FH</li>
-						<li>WPF - FH, SSQ, KB</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Theme]')">Theme</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - SSQ, BG</li>
-						<li>Carbon - SSQ, KB</li>
-						<li>Cocoa - SSQ, KB</li>
-						<li>Motif - SSQ, BG</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - SSQ, FH</li>
-						<li>WPF - SSQ, KB</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Accessibility]')">Accessibility</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - CM, GG</li>
-						<li>Carbon - CM, SSQ</li>
-						<li>Cocoa - SK, CM</li>
-						<li>Motif - CM, SSQ</li>
-						<li>Photon - RD, CM</li>
-						<li>Win32 - CM, GG</li>
-						<li>WPF - CM, KB</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('Printing')">Printing</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - CM, FH</li>
-						<li>Carbon - CM, SSQ</li>
-						<li>Cocoa - KB, SSQ</li>
-						<li>Motif - CM, SSQ</li>
-						<li>Photon - RD, CM</li>
-						<li>Win32 - CM, SSQ</li>
-						<li>WPF - CM, KB, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[DND]')">Drag and Drop</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - KB, BG</li>
-						<li>Carbon - KB, SSQ</li>
-						<li>Cocoa - SK, KB</li>
-						<li>Motif - KB, SSQ</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - KB, FH</li>
-						<li>WPF - KB, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Widgets]')">Widgets</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - BG, SSQ</li>
-						<li>Carbon - SSQ, FH</li>
-						<li>Cocoa - GG, KB, SSQ</li>
-						<li>Motif - BG, SSQ, FH</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - FH, SSQ</li>
-						<li>WPF - KB, FH, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Custom]')">Custom Widgets</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>		
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[WinCE]')">WinCE</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>Win32 - FH, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[OpenGL]')">OpenGL</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - BG, SSQ</li>
-						<li>Carbon - SSQ, KB</li>
-						<li>Cocoa - KB, SSQ</li>
-						<li>Motif - BG, SSQ</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - FH, SSQ</li>
-						<li>WPF - KB, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Program]')">Program</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - BG, SSQ</li>
-						<li>Carbon - SSQ, KB</li>
-						<li>Cocoa - GG, KB</li>
-						<li>Motif - BG, SSQ</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - FH, SSQ</li>
-						<li>WPF - KB, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[OLE]')">OLE/Active X</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>Win32 - FH, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Browser]')">Browser</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - GG, SSQ</li>
-						<li>Carbon - GG, SSQ</li>
-						<li>Cocoa - GG, SSQ</li>
-						<li>Motif - GG, SSQ</li>
-						<li>Photon - RD, GG, SSQ</li>
-						<li>Win32 - GG, SSQ</li>
-						<li>WPF - GG, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[AWT/Swing')">SWT/AWT - Swing</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - SSQ, FH</li>
-						<li>Carbon - SK, SSQ</li>
-						<li>Cocoa - SK, KB, SSQ</li>
-						<li>Motif - SSQ, FH</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - SSQ, FH</li>
-						<li>WPF - KB, SSQ, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[I18N]')">Internationalization</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - FH, SSQ</li>
-						<li>Carbon - FH, SSQ</li>
-						<li>Cocoa - FH, KB</li>
-						<li>Motif - FH, SSQ</li>
-						<li>Photon - RD, SSQ</li>
-						<li>Win32 - FH, SSQ</li>
-						<li>WPF - FH, KB</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[StyledText]')">Styled Text</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - FH, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Image]')">Image Loading</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Layout]')">Layout</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Examples]')">Examples</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[Snippets]')">Snippets</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, FH</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[JUnit]')">JUnit</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>All - CM, GG</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-		<tr>
-			<td>
-				<div id="component">
-				[
-				<a href="javascript:viewBugsWithSummary('[64]')">64 bit</a>
-				]
-				</div>
-			</td>
-			<td>
-				All bugs that concern native library issues.
-			</td>
-			<td>
-				<div class="owner">
-					<ul>
-						<li>GTK - GG, SSQ, BG</li>
-						<li>Carbon - GG, SSQ, KB</li>
-						<li>Cocoa - GG, SSQ, KB</li>
-						<li>Win32 - GG, FH, SSQ</li>
-						<li>WPF - GG, KB, SSQ</li>
-					</ul> 
-				</div>
-			</td>
-		</tr>	
-			
-	</tbody>
+	
 </table>
 </table>
 </body>
