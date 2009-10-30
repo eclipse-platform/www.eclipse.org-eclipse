6552b78cce8122b82cdcebb05fa6432e8fd489f2
ggayed <ggayed>
2009-10-30 20:29:31 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -109,8 +109,7 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
 </table>
 
 <p>The <a href="news://news.eclipse.org/eclipse.platform.swt">SWT newsgroup</a>
-[<a href="http://dev.eclipse.org/newslists/news.eclipse.platform.swt/maillist.html">archive</a>,
-<a href="http://www.eclipse.org/search/search.cgi">search</a>]
+[<a href="http://www.eclipse.org/search/search.cgi">search</a>]
 is a very active user discussion and help forum.
 See the
 <a href="http://www.eclipse.org/newsgroups/index.php">Eclipse newsgroups page</a>
