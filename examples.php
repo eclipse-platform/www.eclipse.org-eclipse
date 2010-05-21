643ef6597d30c1a53b20265eb0eb6fcf9cb10ea7
ggayed <ggayed>
2010-05-21 19:56:41 +0000

    312809 - Examples website has useless links

--- examples.php
+++ examples.php
@@ -53,48 +53,44 @@ and the following examples are <em>only</em> available as Eclipse plugins:
 
 <p>There are several ways to download and run the SWT examples:
 <ul>
-<li><a href="#insideEclipse">Run inside eclipse, either in an example view or from the SWT Example Launcher view.</a></li>
-<li><a href="#standaloneInsideEclipse">Launch as standalone applications from within eclipse.</a></li>
-<li><a href="#standaloneOutsideEclipse">Run as standalone applications outside of eclipse.</a></li>
+<li><a href="#insideEclipse">Run inside Eclipse, either in an example view or from the SWT Example Launcher view.</a></li>
+<li><a href="#standaloneInsideEclipse">Launch as standalone applications from within Eclipse.</a></li>
+<li><a href="#standaloneOutsideEclipse">Run as standalone applications outside of Eclipse.</a></li>
 </ul>
 </p>
 
 <table width="100%">
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">
-<a name="insideEclipse">Running the SWT Examples inside eclipse</a></font>
+<a name="insideEclipse">Running the SWT Examples inside Eclipse</a></font>
 </table>
 
 <ol>
-  <li>To get the examples, go to the eclipse download page: <a href="http://download.eclipse.org/eclipse/downloads/index.php">
-      http://download.eclipse.org/eclipse/downloads/index.php</a></li>
-  <li>Click on the eclipse build that you would like examples for
-      (i.e. the eclipse build that you are running; typically the latest Stable Build).</li>
+  <li>To get the examples, go to the Eclipse downloads page at <a href="http://download.eclipse.org/eclipse/downloads/index.php">
+      http://download.eclipse.org/eclipse/downloads/index.php</a>.</li>
+  <li>Click on the Eclipse build that you would like examples for
+      (i.e. the Eclipse build that you are running; typically the latest Stable Build).</li>
   <li>Scroll down until you see "Example Plug-ins". Read the paragraph on installing them, and select the download for your platform.</li>
-  <li>After installing the examples, the source for them will be in a src.zip file in the
-      appropriate subdirectory of: eclipse/plugins/org.eclipse.sdk.examples.source_&lt;version&gt;/src/
-      <br>For example, the 3.1 source for the standalone examples in org.eclipse.swt.examples is in: org.eclipse.sdk.examples.source_3.1.0/src/org.eclipse.swt.examples_3.1.0/swtexamplessrc.zip.</li>
-      <br>The examples in package org.eclipse.swt.examples run standalone,
-and the examples in org.eclipse.swt.examples.* packages are eclipse plug-ins.
-  <li>To run the plug-in SWT examples, restart eclipse, go to <i>Window &gt; Show View... &gt; Other...</i> and expand "SWT Examples".</li>
+  <li>After installing the examples, you can find their source in <code>eclipse/plugins/org.eclipse.swt.examples.&lt;exampleName&gt;.source_&lt;version&gt;.jar</code>.
+  <li>To run the plug-in SWT examples, restart Eclipse, go to <i>Window &gt; Show View... &gt; Other...</i> and expand "SWT Examples".</li>
 </ol>
 
-<p>Alternatively, you can use these (more detailed) steps from the eclipse ISV doc to download, install, and run
-the eclipse SWT Examples:
+<p>Alternatively, you can use these (more detailed) steps from the Eclipse ISV doc to download, install, and run
+the Eclipse SWT Examples:
 <ol>
-<li><a href = "http://help.eclipse.org/stable/topic/org.eclipse.platform.doc.isv/samples/samples.html">
+<li><a href = "http://help.eclipse.org/galileo/topic/org.eclipse.platform.doc.isv/samples/samples.html">
 	Installing the examples</a></li>
-<li><a href = "http://help.eclipse.org/stable/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples.launcher/doc-html/swt_launcher_ex.html">
+<li><a href = "http://help.eclipse.org/galileo/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples.launcher/doc-html/swt_launcher_ex.html">
 	Running the SWT examples as views or with the SWT Example Launcher</a></li>
-<li><a href = "http://help.eclipse.org/stable/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples/doc-html/swt_manual_setup.html">
+<li><a href = "http://help.eclipse.org/galileo/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples/doc-html/swt_manual_setup.html">
 	Running the SWT standalone examples</a></li>
-<li><a href = "http://help.eclipse.org/stable/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples/doc-html/swt_manual_setup.html#ExamplesOverview">
+<li><a href = "http://help.eclipse.org/galileo/topic/org.eclipse.platform.doc.isv/samples/org.eclipse.swt.examples/doc-html/swt_manual_setup.html#ExamplesOverview">
 	SWT Examples Overview</a></li>
 </ol>
 </p>
 
 <table width="100%">
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">
-<a name="standaloneInsideEclipse">Launching standalone SWT Examples inside eclipse</a></font>
+<a name="standaloneInsideEclipse">Launching standalone SWT Examples inside Eclipse</a></font>
 </table>
 
 <p></p>
@@ -102,51 +98,32 @@ the eclipse SWT Examples:
   <li><a href="eclipse.php">Load SWT into your workspace.</a></li>
   <li><a href="#insideEclipse">Load the SWT examples into your workspace by one of the methods listed above</a>.</li>
   <li>Run a standalone example by selecting the main class (e.g. org.eclipse.swt.examples.controlexample.ControlExample) and selecting
-	<br><strong>Run &gt; Run As &gt; Java Application</strong> (Note: prior to Eclipse 3.3 use
-	<strong>Run &gt; Run As &gt; SWT Application</strong>).</li>
+	<br><em>Run &gt; Run As &gt; Java Application</em> (Note: prior to Eclipse 3.3 use
+	<em>Run &gt; Run As &gt; SWT Application</em>).</li>
 </ol>
 
 <table width="100%">
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">
-<a name="standaloneOutsideEclipse">Running standalone SWT Examples outside of eclipse</a></font>
+<a name="standaloneOutsideEclipse">Running standalone SWT Examples outside of Eclipse</a></font>
 </table>
 
-<p>To run the ControlExample and CustomControlExample standalone: (NOTE: these instructions will only work for eclipse 3.1 and newer).  </p>
+<p>(NOTE: these instructions will only work for Eclipse 3.3 and newer) To run the ControlExample or CustomControlExample standalone:</p>
 <ol>
-<li>Go to the download page for the eclipse version you are running.
-   For example, for eclipse 3.4.2 this is:<br>
-   <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.4.2-200902111700/index.php">http://download.eclipse.org/eclipse/downloads/drops/R-3.4.2-200902111700/index.php</a>.
-   </li>
+  <li>Go to the Eclipse downloads page at <a href="http://download.eclipse.org/eclipse/downloads/index.php">
+      http://download.eclipse.org/eclipse/downloads/index.php</a>.</li>
+  <li>Click on the Eclipse build that you would like examples for
+      (i.e. the Eclipse/SWT build that you are running; typically the latest Stable Build).</li>
+  <li>Scroll down until you see "Example Plug-ins", download the .zip for your platform, and extract it to your local machine.</li>
+  <li>In the directory with the extracted example plug-ins, find file <code>org.eclipse.swt.examples_&lt;version&gt;.jar</code>, and extract it in place.</li>
+  <li>Open a DOS window (on Windows) or a terminal window (on Mac or Linux) and navigate to this directory.</li>
+  <li>To run the example, type the following line with the appropriate paths substituted:<br>
 
-<li>Scroll down to the "Example Plug-ins" section, download the examples zip for your platform, and unzip
-   it to the same place that you originally unzipped eclipse (answer Yes when it asks if you want to overwrite the license files).
-   Let's say that you unzip both eclipse and its examples to d:\
-   </li>
-
-<li>(NOTE: If you are using eclipse 3.3 or newer then you can skip this step)<br>
-   Open the file d:\eclipse\plugins\org.eclipse.swt.win32.win32.x86_3.X.X.jar
-   in winzip and extract the following 3 files into d:\eclipse\plugins
-   
-   <ul>
-   <li>swt-awt-win32-XXXX.dll
-   <li>swt-gdip-win32-XXXX.dll
-   <li>swt-win32-XXXX.dll
-   </ul></li>
-
-<li>Open a DOS window (on Windows) or a terminal window (on Mac or Linux) and navigate to the examples plugin directory, for example on Windows, type:<br>
-   <code>d:<br>
-   cd d:\eclipse\plugins\org.eclipse.swt.examples_3.X.X</code></li>
-
-<li>To run the example, type the following line: (NOTE: Change the "win32.win32.x86" to your platform).<br>
-
-<code><i>&lt;pathToYourJRE&gt;</i>\bin\java -classpath .;..\org.eclipse.swt_3.X.X.jar;..\org.eclipse.swt.win32.win32.x86_3.X.X.jar;.\swtexamples.jar org.eclipse.swt.examples.controlexample.ControlExample</code>
-
-<br>(NOTE: If you are using eclipse 3.2.2 or earlier, you also need <code>-Djava.library.path=..</code>)
+<code>&lt;pathToYourJRE&gt;\bin\java -classpath &lt;pathOfYourPlatformSWTjar&gt;;.\swtexamples.jar org.eclipse.swt.examples.controlexample.ControlExample</code>
 
 <p>This is the ControlExample. There is a tab for each control, and you can
 change the various styles, etc, to see what changes in the controls. To run
 the CustomControlExample simply use the class name CustomControlExample
-instead of ControlExample in step 5.</p></li>
+instead of ControlExample in step 6.</p></li>
 
 </ol>
 
