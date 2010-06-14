b73f88b324e99424535c3b4f6496207e0c398dc0
carolyn <carolyn>
2010-06-14 18:29:12 +0000

    new javadoc page (just points to swt packages in eclipse javadoc pages)

---
+++ javadoc.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>SWT Javadoc</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+    <link rel="stylesheet" href="swt.css" type="text/css">
+    <link rel="shortcut icon" href="http://www.eclipse.org/images/eclipse.ico" type="image/x-icon">
+</head>
+<body bgcolor="#ffffff" text="#000000">
+<table width="635px" class="swtpage">
+<colgroup><col width="125px"><col width="510px"></colgroup>
+<tr><?php include "sidebar.php"; ?>
+<h1 style="padding: 0; margin: 0; border-bottom: 1px solid #000000;">SWT Javadoc</h1>
+
+<p>Online javadoc for the SWT project.</p>
+
+<tbody><tr class="TableHeadingColor" bgcolor="#ccccff">
+<th colspan="2" align="left"><font size="+1">
+<b>Packages</b></font></th>
+</tr>
+
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/package-summary.html">org.eclipse.swt</a></b></td>
+
+<td>SWT constants and error handling support.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/accessibility/package-summary.html">org.eclipse.swt.accessibility</a></b></td>
+<td>SWT Accessibility.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/awt/package-summary.html">org.eclipse.swt.awt</a></b></td>
+<td>SWT AWT embedding support.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/browser/package-summary.html">org.eclipse.swt.browser</a></b></td>
+
+<td>SWT Browser widget.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/custom/package-summary.html">org.eclipse.swt.custom</a></b></td>
+<td>SWT Custom widgets.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/dnd/package-summary.html">org.eclipse.swt.dnd</a></b></td>
+<td>SWT Drag and Drop support.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/events/package-summary.html">org.eclipse.swt.events</a></b></td>
+
+<td>SWT typed listener support.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/graphics/package-summary.html">org.eclipse.swt.graphics</a></b></td>
+<td>SWT graphics classes.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/layout/package-summary.html">org.eclipse.swt.layout</a></b></td>
+<td>SWT layout classes.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/ole/win32/package-summary.html">org.eclipse.swt.ole.win32</a></b></td>
+
+<td>SWT Win32 OLE implementation classes.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/opengl/package-summary.html">org.eclipse.swt.opengl</a></b></td>
+<td>SWT OpenGL support.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/printing/package-summary.html">org.eclipse.swt.printing</a></b></td>
+<td>SWT Printing support classes.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/program/package-summary.html">org.eclipse.swt.program</a></b></td>
+
+<td>SWT Program support class.</td>
+</tr>
+<tr class="TableRowColor" bgcolor="white">
+<td width="30%"><b><a href="http://help.eclipse.org/helios/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/widgets/package-summary.html">org.eclipse.swt.widgets</a></b></td>
+<td>SWT widget public API classes.</td>
+</tr>
+</tbody>
+
+</table>
+</body>
+</html>
