10e766236f629db3a9c3a311e5bc0c18a196fac9
ggayed <ggayed>
2010-06-24 13:56:20 +0000

    3.6 release

--- index.php
+++ index.php
@@ -54,12 +54,12 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
   <table width="100%">
   <tr><td>
       <b><font size="+1">Development</font></b><br>
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.6RC4-201006031500/download.php?dropFile=swt-3.6RC4-win32-win32-x86.zip">Windows</a>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.6RC4-201006031500/download.php?dropFile=swt-3.6RC4-gtk-linux-x86.zip">Linux</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/download.php?dropFile=swt-3.6-win32-win32-x86.zip">Windows</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/download.php?dropFile=swt-3.6-gtk-linux-x86.zip">Linux</a>,
       <span style="white-space: nowrap;">
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.6RC4-201006031500/download.php?dropFile=swt-3.6RC4-cocoa-macosx.zip">OS X</a></span>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.6RC4-201006031500/index.php#swt" target="_top">more...</a><br>
-      <small>3.6 Release Candidate 4 - <span style="white-space: nowrap;">4 June 2010</span></small><br>
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/download.php?dropFile=swt-3.6-cocoa-macosx.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/index.php#swt" target="_top">more...</a><br>
+      <small>3.6 Final Release - <span style="white-space: nowrap;">23 June 2010</span></small><br>
   <ul>
   <li><a href="R3_6/plan.html">3.6 Development Plan</a></li>
   <li><a href="R3_6/new_and_noteworthy.html">New and Noteworthy</a></li>
