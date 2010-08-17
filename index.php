ad50513da29bbff9ba51d4558f3900636dec5486
ggayed <ggayed>
2010-08-17 20:45:59 +0000

    update for start of 3.7

--- index.php
+++ index.php
@@ -39,21 +39,6 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
   <table width="100%">
   <tr><td>
       <b><font size="+1">Stable</font></b><br>
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5.2-201002111343/download.php?dropFile=swt-3.5.2-win32-win32-x86.zip">Windows</a>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5.2-201002111343/download.php?dropFile=swt-3.5.2-gtk-linux-x86.zip">Linux</a>,
-      <span style="white-space: nowrap;">
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5.2-201002111343/download.php?dropFile=swt-3.5.2-cocoa-macosx.zip">OS X</a></span>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.5.2-201002111343/index.php#SWT" target="_top">more...</a><br>
-      <small>3.5.2 Final Release - <span style="white-space: nowrap;">26 February 2010</span></small><br>
-  <ul>
-  <li><a href="R3_5/new_and_noteworthy.html">New and Noteworthy</a></li>
-  <li><a href="language.php">Language Pack</a></li>
-  </ul>
-  </table>
-<td width="50%" valign="top">
-  <table width="100%">
-  <tr><td>
-      <b><font size="+1">Development</font></b><br>
       <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/download.php?dropFile=swt-3.6-win32-win32-x86.zip">Windows</a>,
       <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/download.php?dropFile=swt-3.6-gtk-linux-x86.zip">Linux</a>,
       <span style="white-space: nowrap;">
@@ -61,8 +46,23 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
       <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.6-201006080911/index.php#SWT" target="_top">more...</a><br>
       <small>3.6 Final Release - <span style="white-space: nowrap;">23 June 2010</span></small><br>
   <ul>
-  <li><a href="R3_6/plan.html">3.6 Development Plan</a></li>
   <li><a href="R3_6/new_and_noteworthy.html">New and Noteworthy</a></li>
+  <li><a href="language.php">Language Pack</a></li>
+  </ul>
+  </table>
+<td width="50%" valign="top">
+  <table width="100%">
+  <tr><td>
+      <b><font size="+1">Development</font></b><br>
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.7M1-201008051700/download.php?dropFile=swt-3.7M1-win32-win32-x86.zip">Windows</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.7M1-201008051700/download.php?dropFile=swt-3.7M1-gtk-linux-x86.zip">Linux</a>,
+      <span style="white-space: nowrap;">
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.7M1-201008051700/download.php?dropFile=swt-3.7M1-cocoa-macosx.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.7M1-201008051700/index.php#SWT" target="_top">more...</a><br>
+      <small>3.7 Milestone 1 - <span style="white-space: nowrap;">6 August 2010</span></small><br>
+  <ul>
+<!--  <li><a href="R3_7/plan.html">3.7 Development Plan</a></li> -->
+  <li><a href="R3_7/new_and_noteworthy.html">New and Noteworthy</a></li>
 
   </ul>
   </table>
