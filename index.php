c1929e3cfea95aa4ea29dae08027816f3553e0ad
ggayed <ggayed>
2010-08-18 14:20:21 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ on which it is implemented.</p>
 <tr><th bgcolor="#0080c0" align="left" style="padding: 2px;"><font color="#ffffff">Welcome</font>
 </table>
 
-<p>Welcome to SWT!!! You may have come to this page looking for <a href="http://www.eclipse.org/swt/snippets/">snippets</a> (little code examples)
+<p>Welcome to SWT! You may have come to this page looking for <a href="http://www.eclipse.org/swt/snippets/">snippets</a> (little code examples)
 or <a href="http://www.eclipse.org/swt/examples.php">examples</a> (big code examples like <em>ControlExample</em>);
 <a href="http://www.eclipse.org/swt/widgets/">widget snapshots</a>
 or <a href="http://www.eclipse.org/swt/docs.php">documentation</a>.
@@ -61,7 +61,6 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
       <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.7M1-201008051700/index.php#SWT" target="_top">more...</a><br>
       <small>3.7 Milestone 1 - <span style="white-space: nowrap;">6 August 2010</span></small><br>
   <ul>
-<!--  <li><a href="R3_7/plan.html">3.7 Development Plan</a></li> -->
   <li><a href="R3_7/new_and_noteworthy.html">New and Noteworthy</a></li>
 
   </ul>
