a9fb8b069a5c086a3570d1243178f5a9cbeeed2d
ggayed <ggayed>
2011-03-04 17:05:05 +0000

    *** empty log message ***

---
+++ repeat.js
@@ -0,0 +1,22 @@
+function theFunctionTwo(z) {
+   var d = 2;
+   var e = 'asdf';
+   var f = null;
+   var g = true;
+   var h = false;
+   var i = 3.567;
+   var j = 'jkl;';
+   var k = '';
+   var l = window;
+   var m = [3];
+   var n = document;
+   var o = l;
+}
+function theFunctionOne() {
+   setTimeout("theFunctionOne()", 5000);
+   var a = 2;
+   var b = 2;
+   theFunctionTwo(3);
+   alert("hi");
+   var c = 2;
+}
