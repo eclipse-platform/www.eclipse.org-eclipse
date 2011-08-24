e3e9627dcf505cd40a55fadee9d687160f7885b4
ggayed <ggayed>
2011-08-24 15:34:13 +0000

    provide link

--- index.php
+++ index.php
@@ -53,14 +53,14 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
 <td width="50%" valign="top">
   <table width="100%">
   <tr><td>
-<!-- (commented out until 3.8M1)
       <b><font size="+1">Development</font></b><br>
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.7-201106131736/download.php?dropFile=swt-3.7-win32-win32-x86.zip">Windows</a>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.7-201106131736/download.php?dropFile=swt-3.7-gtk-linux-x86.zip">Linux</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-win32-win32-x86.zip">Windows</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-gtk-linux-x86.zip">Linux</a>,
       <span style="white-space: nowrap;">
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.7-201106131736/download.php?dropFile=swt-3.7-cocoa-macosx.zip">OS X</a></span>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.7-201106131736/index.php#SWT" target="_top">more...</a><br>
-      <small>3.7 Final Release - <span style="white-space: nowrap;">13 June 2011</span></small><br>
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-cocoa-macosx.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/index.php#SWT" target="_top">more...</a><br>
+      <small>3.8 Milestone 1 - <span style="white-space: nowrap;">3 August 2011</span></small><br>
+<!--
   <ul>
   <li><a href="R3_7/plan.html">3.7 Development Plan</a></li>
   <li><a href="R3_7/new_and_noteworthy.html">New and Noteworthy</a></li>
