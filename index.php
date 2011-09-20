6c5ee79375f6f64fd58ce8413ceaed1b3f96d2bd
gheorghe <gheorghe>
2011-09-20 16:11:49 +0000

    Updated for M2

--- index.php
+++ index.php
@@ -54,18 +54,17 @@ Whatever the reason, you will find many resources here. Enjoy!</p>
   <table width="100%">
   <tr><td>
       <b><font size="+1">Development</font></b><br>
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-win32-win32-x86.zip">Windows</a>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-gtk-linux-x86.zip">Linux</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M2-201109151308/download.php?dropFile=swt-3.8M2-win32-win32-x86.zip">Windows</a>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M2-201109151308/download.php?dropFile=swt-3.8M2-gtk-linux-x86.zip">Linux</a>,
       <span style="white-space: nowrap;">
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/download.php?dropFile=swt-3.8M1-cocoa-macosx.zip">OS X</a></span>,
-      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M1-201108031800/index.php#SWT" target="_top">more...</a><br>
-      <small>3.8 Milestone 1 - <span style="white-space: nowrap;">3 August 2011</span></small><br>
-<!--
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M2-201109151308/download.php?dropFile=swt-3.8M2-cocoa-macosx.zip">OS X</a></span>,
+      <a href="http://download.eclipse.org/eclipse/downloads/drops/S-3.8M2-201109151308/index.php#SWT" target="_top">more...</a><br>
+      <small>3.8 Milestone 2 - <span style="white-space: nowrap;">16 September 2011</span></small><br>
   <ul>
-  <li><a href="R3_7/plan.html">3.7 Development Plan</a></li>
-  <li><a href="R3_7/new_and_noteworthy.html">New and Noteworthy</a></li>
+  <li><a href="R3_8/plan.html">3.8 Development Plan</a></li>
+  <!--<li><a href="R3_7/new_and_noteworthy.html">New and Noteworthy</a></li>-->
   </ul>
--->
+
   </table>
 </table>
 
