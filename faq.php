6437797c4fd9b0deac077176938b31a2b56d14ae
Eric Williams <ericwill@redhat.com>
2018-11-08 13:48:57 -0500

    Bug 540932: [GTK3] Remove GTK_ACCEL_LABEL macros
    Clean up SWT FAQ page wrt GTK versions.
    
    Change-Id: Ifb628b9f09f88411d32c5359a61f0cba190affff
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- faq.php
+++ faq.php
@@ -365,6 +365,8 @@ the <a href="http://dev.eclipse.org/mailman/listinfo/platform-swt-dev">SWT devel
   <dt><strong><a name="gtkstartup">Q: Which GTK version do I need to run SWT?</a></strong></dt>
   <dd>A: SWT requires the following GTK+ versions (or newer) to be installed:
     <ul>
+      <li>Eclipse/SWT 4.10.x: GTK+ 3.6.0 and its dependencies</li>
+      <li>Eclipse/SWT 4.6.x - 4.9.x: GTK+ 2.24.0 and its dependencies (for GTK+ 2) OR GTK+ 3.0.0 and its dependencies (for GTK+ 3)</li>
       <li>Eclipse/SWT 4.5.x and newer: GTK+ 2.18.0 and its dependencies (for GTK+ 2) OR GTK+ 3.0.0 and its dependencies (for GTK+ 3)</li>
       <li>Eclipse/SWT 4.4.x: GTK+ 2.10.0 and its dependencies (for GTK+ 2) OR GTK+ 3.0.0 and its dependencies (for GTK+ 3)</li>
       <li>Eclipse/SWT 4.3.x: GTK+ 2.10.0 and its dependencies</li>
@@ -373,11 +375,12 @@ the <a href="http://dev.eclipse.org/mailman/listinfo/platform-swt-dev">SWT devel
       <li>Eclipse/SWT 3.0.x - 3.5.x: GTK+ 2.2.1 and its dependencies</li>
       <li>Eclipse/SWT 2.1.x: GTK+ 2.0.6 and its dependencies</li>
     </ul>
-    Note that Eclipse/SWT 4.3.x includes early access support for GTK+ 3.x.  To use it on a Linux distro with GTK+ 3.x
-    libraries installed, set Linux environment variable SWT_GTK3=1 before launching your application.
+    Note that Eclipse/SWT 4.3.x includes early access support for GTK+ 3.x.
     <p>
     Starting from Eclipse/SWT 4.4.x, Linux builds come with GTK+ 3 support enabled by default.
-    You can force Eclipse/SWT to use GTK+ 2 by setting the environment variable SWT_GTK3 to 0, if needed. 
+    </p>
+    <p>
+    Starting from Eclipse/SWT 4.10.x, Linux builds only come with GTK+ 3 support, GTK+ 2 is no longer supported.
     </p>
     <p>
     You can determine which version(s) of GTK you have installed with <tt>rpm -q gtk2</tt> or <tt>rpm -q gtk3</tt>.
