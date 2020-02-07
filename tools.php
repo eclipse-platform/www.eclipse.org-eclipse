b45852684def6aab979a8e47f0b0746c672a5752
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2020-02-07 17:55:19 +0530

    Bug 528173 - SWT Website Documentation needs a refresh
    Cleanup SWT tools page and update links. Remove old jars.
    
    Change-Id: I076af591b2b1575a13aad808bb64d9f1017dbda1

--- tools.php
+++ tools.php
@@ -7,7 +7,7 @@ ob_start();
 <div id="midcolumn">
 <h1>SWT Development Tools</h1>
 
-<p>The SWT Development Tools are available from the <a href="updatesite.php">SWT Tools Update Sites</a></p>
+<p>The SWT Development Tools described in this page are available from the <a href="updatesite.php">SWT Tools Update Sites</a></p>
 
 <p><b>JniGen</b> is a tool that generates JNI wrappers for creating the SWT Platform Interface (PI).
 For more information on how to generate the JNI code, see <a href="jnigen.php">this tutorial</a>.</p>
@@ -15,14 +15,7 @@ For more information on how to generate the JNI code, see <a href="jnigen.php">t
 <p><b>Sleak</b> is a simple tool that monitors the creation and disposal of SWT graphics resources.
 (<a href="http://www.eclipse.org/articles/swt-design-2/sleak.htm">see tutorial</a>)</p>
 <ul>
-  <li><b>Sleak plug-in for Eclipse 2.1.0 to 3.3</b> <a href="tools/org.eclipse.swt.sleak_1.0.0.zip">(zip file)</a>
-    <ul><li>Extract the zip file into the <code>eclipse/plugins/</code> folder.</li></ul></li>
-  <li><b>Sleak plug-in for Eclipse 3.4 M1 - M5</b> <a href="tools/org.eclipse.swt.sleak_1.0.1.zip">(zip file)</a>
-    <ul><li>Extract the zip file into the <code>eclipse/plugins/</code> folder.</li></ul></li>
-  <li><b>Sleak plug-in for Eclipse 3.4 M6 - 3.5</b> <a href="tools/org.eclipse.swt.sleak_1.0.2.zip">(zip file)</a>
-    <ul><li>Extract the zip file into the <code>eclipse/dropins/</code> folder.</li></ul></li>
-  <li><b>Sleak plug-in for Eclipse 3.5 or later</b> 
-    <ul><li>Available from the <a href="updatesite.php">SWT Tools Update Sites</a>.</li></ul></li>
+  <li><b>Install Sleak plug-in</b> from <a href="updatesite.php">SWT Tools Update Sites</a></li>
 </ul>
 <ul>
   <li><b>After installing the Sleak plug-in (above):</b></li>
@@ -37,7 +30,7 @@ For more information on how to generate the JNI code, see <a href="jnigen.php">t
       <li>The plug-in monitors all other plug-ins running in its Eclipse environment.</li>
     </ul>
 </ul><ul>
-  <li><b>Sleak for standalone applications </b><a href="http://git.eclipse.org/c/platform/eclipse.platform.swt.git/tree/bundles/org.eclipse.swt.tools/Sleak/org/eclipse/swt/tools/internal/Sleak.java">(Sleak.java)</a></li>
+  <li><b>Sleak for standalone applications </b><a href="https://git.eclipse.org/c/platform/eclipse.platform.swt.git/tree/bundles/org.eclipse.swt.tools.spies/src/org/eclipse/swt/tools/internal/Sleak.java">(Sleak.java)</a></li>
 </ul>
 
 <p><b>SWT Spy plug-in</b> for Eclipse is a simple tool that prints out
@@ -45,15 +38,9 @@ information about the widget under the cursor.  Currently, this includes
 style, layout and parent information.</p>
 
 <ul>
-  <li>Requires Eclipse 3.0 or later.</li>
-  <li>As of Eclipse 3.5, this is available from the <a href="updatesite.php">SWT Tools Update Sites</a>.</li>
-  <li>Prior to Eclipse 3.5,
-	<ul>
-	  <li>Save <a href="tools/org.eclipse.swt.spy_1.0.1.jar">org.eclipse.swt.spy_1.0.1.jar</a>
-      into the <code>eclipse/plugins/</code> folder.</li>
-	  <li>Launch Eclipse with "-clean" option (clears caches so Eclipse will pick up new plugin).</li>
-	</ul></li>
-</ul><ul>
+  <li>Install Spy plug-in from <a href="updatesite.php">SWT Tools Update Sites</a>.</li>
+</ul>
+<ul>
   <li><b>After installing the Spy plug-in (above):</b></li>
   <ul>
     <li>Open the SWT Spy view (Window &gt; Show View &gt; Other ... &gt; SWT Tools &gt; SWT Spy).</li>
