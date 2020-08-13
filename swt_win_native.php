f1fca247c6a8b966621bc277bb6979ad1d2831df
Lars Vogel <Lars.Vogel@vogella.com>
2020-08-13 14:41:21 +0200

    Bug 566036 - Remove 32bit instructions and WebKit from https://www.eclipse.org/swt/swt_win_native.php
    Change-Id: Ief8a679d4085ec88693665769c8aa9a2779d246d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- swt_win_native.php
+++ swt_win_native.php
@@ -11,17 +11,7 @@ ob_start();
 <h3>Software needed for setting up windows native build</h3>
 <ol>
 <li><p> Microsoft 'Visual Studio - Community 2017' and 'Windows 10 SDK', link to <a href="https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15">download page</a></p>
-<li><p> JDK8 64bit and 32bit, link to <a href="http://www.oracle.com/technetwork/java/javase/overview/java8-2100321.html">Oracle JDK</a> or <a href="https://developer.ibm.com/javasdk/downloads/">IBM JDK</a></p>
-<li><p> Webkit SDKs:</p>
- <ol>
-	<li><p><a href="http://build.eclipse.org/eclipse/swt/WebKit-r72896.zip">WebKit-r72896</a></p>
-	<li><p><a href="http://build.eclipse.org/eclipse/swt/WebKitSupportLibrary.zip">WebKitSupportLibrary</a></p>
- </ol>
-<p><b>Disclaimer:</b></p>
-<p><b>Note these Webkit SDKs are an older version being consumed by SWT native build process and
- Webkit build binaries are no more available for download in public <a href="https://webkit.org/downloads/">Webkit download page</a>.</b></p>
-<p><b>But WebKit-r72896 sources are still avilable for download in public <a href="https://builds-nightly.webkit.org/files/trunk/src/WebKit-r72896.tar.bz2">main link</a>
-and also on <a href="http://build.eclipse.org/eclipse/swt/WebKit-r72896.tar.bz2">backup link</a>.</b></p>
+<li><p> JDK8 64bit link to <a href="http://www.oracle.com/technetwork/java/javase/overview/java8-2100321.html">Oracle JDK</a> or <a href="https://developer.ibm.com/javasdk/downloads/">IBM JDK</a></p>
 <li> Cygwin [Optional for local setup]</p>
 </ol>
 <p>Note: [Moved to latest "Win10 SDK" and "Visual Studio 2017 community edition" 
@@ -42,25 +32,16 @@ via bugzilla <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=526802">bug 
 <li><p> Copy the public keys and register then on the Hudson machine.. this should enable password-less authentication.</p>
 </ul>
 
-<li><p> 'SWT_BUILDDIR' root directory contain various libraries like Visual Studio libraries, JDK and libraries for compiling Webkit etc.. </p>
+<li><p> 'SWT_BUILDDIR' root directory contain various libraries like Visual Studio libraries, JDK, etc.. </p>
 <p> Setup your own 'SWT_BUILDDIR' with below like directory structure:</p>
 <ul>
 <li><p> Install Visual Studio Community 2017 libraries in default location or in: 'SWT_BUILDDIR\Microsoft\Visual Studio\2017'</p>
 <li><p> Windows10 SDK gets installed in "Program Files (x86)" directory by default like: 'C:\Program Files (x86)\Windows Kits\10'</p>
-<li><p> Unzip the two Webkit SDKs into 'SWT_BUILDDIR\Webkit' as below directories:</p>
-	<ol>
-	<li><p>'SWT_BUILDDIR\Webkit\r72896'</p>
-	<li><p>'SWT_BUILDDIR\Webkit\SupportLibrary'</p>
-	</ol>
-	<p>Note: SWT's Webkit support exists for SWT 32bit on Windows, so Webkit SDKs are consumed only by the SWT 32bit build process.</p>
 <li><p> Install/Unzip JDK8 64bit in 'SWT_BUILDDIR\Java\Oracle\jdk1.8.0-latest\x64'</p>
-<li><p> Install/Unzip JDK8 32bit in 'SWT_BUILDDIR\Java\Oracle\jdk1.8.0-latest\x86'</p>
 </ul>
 
 <li> <p> For local testing, Run as Ant build below file with 'build_libraries' as target(assuming <a href="git.php">SWT setup</a> is in place):</p>
 <p>${workspace_loc:/org.eclipse.swt.win32.win32.x86_64/build.xml}</p>
-<p> For building windows native build for 32bit, you need to convert the source to 32bit first, by running 'replace.64.to.32' target</p>
-<p> Once sources are converted to 32bit you can run the 'build_libraries' target for ${workspace_loc:/org.eclipse.swt.win32.win32.x86/build.xml}</p>
 <li>[Optional for local setup] Now you can point the Windows hudson job to this machine and trigger a native build.
 <p> Note: For testing purpose from hudson, temporarily turn the nativeChanges flag to 'true' to force a native build compilation.</p>
 </ol>
