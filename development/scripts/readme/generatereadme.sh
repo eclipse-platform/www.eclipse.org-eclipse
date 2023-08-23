ac5f98a3ab4b11887d610ab6a63f94224d4e08a8
Sheena <122248524+Shee43@users.noreply.github.com>
2023-08-23 15:18:33 +0530

    Readme file for 4.29 (#55)

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.27/4\.28/g'  readme_eclipse.html
-sed -i -e 's/4_27/4_28/g' readme_eclipse.html
-sed -i -e 's/4\.26/4\.27/g' readme_eclipse.html
+sed -i -e 's/4\.28/4\.29/g'  readme_eclipse.html
+sed -i -e 's/4_28/4_29/g' readme_eclipse.html
+sed -i -e 's/4\.27/4\.28/g' readme_eclipse.html
 
