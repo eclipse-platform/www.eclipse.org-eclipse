ba44a74aab5a2b9a5156350b218f03a8f42e870f
Suby S Surendran <suby.surendran@ibm.com>
2024-02-25 13:31:14 +0530

    ReadMe for Eclipse 4.31
    Add the ReadMe infrastructure for Eclipse 4.31

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.29/4\.30/g'  readme_eclipse.html
-sed -i -e 's/4_29/4_30/g' readme_eclipse.html
-sed -i -e 's/4\.28/4\.29/g' readme_eclipse.html
+sed -i -e 's/4\.30/4\.31/g'  readme_eclipse.html
+sed -i -e 's/4_30/4_31/g' readme_eclipse.html
+sed -i -e 's/4\.29/4\.30/g' readme_eclipse.html
 
