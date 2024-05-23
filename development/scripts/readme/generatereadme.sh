aed477f807bff910e76fcdd0da9b25cbb2dd0828
Suby S Surendran <suby.surendran@ibm.com>
2024-05-23 11:28:03 +0530

    ReadMe for Eclipse 4.32

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.30/4\.31/g'  readme_eclipse.html
-sed -i -e 's/4_30/4_31/g' readme_eclipse.html
-sed -i -e 's/4\.29/4\.30/g' readme_eclipse.html
+sed -i -e 's/4\.31/4\.32/g'  readme_eclipse.html
+sed -i -e 's/4_30/4_32/g' readme_eclipse.html
+sed -i -e 's/4\.30/4\.31/g' readme_eclipse.html
 
