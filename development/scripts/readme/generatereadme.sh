0544729077b916c8f612e8bb45646bc5fa406bdb
sougandhs <sougandh.s@ibm.com>
2024-08-23 17:33:23 +0530

    Track - https://github.com/eclipse-platform/www.eclipse.org-eclipse/issues/207

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.31/4\.32/g'  readme_eclipse.html
-sed -i -e 's/4_30/4_32/g' readme_eclipse.html
-sed -i -e 's/4\.30/4\.31/g' readme_eclipse.html
+sed -i -e 's/4\.32/4\.33/g'  readme_eclipse.html
+sed -i -e 's/4_32/4_33/g' readme_eclipse.html
+sed -i -e 's/4\.32/4\.33/g' readme_eclipse.html
 
