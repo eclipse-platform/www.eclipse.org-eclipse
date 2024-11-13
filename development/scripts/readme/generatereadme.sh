2913418dec8bf9c14200f9d2a0439ee5b9bdcb1c
sougandhs <sougandh.s@ibm.com>
2024-11-13 13:48:59 +0530

    Read me changes for 4.34 release #248
    This commit includes read me changes shell script changes for eclipse
    4.34 release.
    
    Enhancement :
    https://github.com/eclipse-platform/www.eclipse.org-eclipse/issues/248

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.32/4\.33/g'  readme_eclipse.html
-sed -i -e 's/4_32/4_33/g' readme_eclipse.html
-sed -i -e 's/4\.32/4\.33/g' readme_eclipse.html
+sed -i -e 's/4\.33/4\.34/g'  readme_eclipse.html
+sed -i -e 's/4_33/4_34/g' readme_eclipse.html
+sed -i -e 's/4\.33/4\.34/g' readme_eclipse.html
 
