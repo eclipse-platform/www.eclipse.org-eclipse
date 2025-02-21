53d2b7e137726d1dc3bc5c591c8773da0c2376ae
sougandhs <sougandh.s@ibm.com>
2025-02-21 11:13:25 +0530

    Read me changes for 4.35 release
    This commit includes read me changes & shell script changes for eclipse
    4.35 release.
    
    Fixes : https://github.com/eclipse-platform/www.eclipse.org-eclipse/issues/279

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.33/4\.34/g'  readme_eclipse.html
-sed -i -e 's/4_33/4_34/g' readme_eclipse.html
-sed -i -e 's/4\.33/4\.34/g' readme_eclipse.html
+sed -i -e 's/4\.34/4\.35/g'  readme_eclipse.html
+sed -i -e 's/4_34/4_35/g' readme_eclipse.html
+sed -i -e 's/4\.34/4\.35/g' readme_eclipse.html
 
