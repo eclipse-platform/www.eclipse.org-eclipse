c11281af1e80d82cf857600134b417d509e89d07
Ed Merks <ed.merks@gmail.com>
2025-04-04 14:29:14 +0200

    Fix Javadoc.html -> javadoc.html

--- swt/project.js
+++ swt/project.js
@@ -28,7 +28,7 @@ defaultAside = toElements(`
 <a href="${swtScriptBase}examples.html">Examples</a>
 <a href="${swtScriptBase}faq.html">FAQ</a>
 <a href="${swtScriptBase}tools.html">Tools</a>
-<a href="${swtScriptBase}Javadoc.html">Javadoc</a>
+<a href="${swtScriptBase}javadoc.html">Javadoc</a>
 <a href="${swtScriptBase}docs.html">Documentation</a>
 <a href="${swtScriptBase}community.html">Community</a>
 <a href="${swtScriptBase}index.html#contact">Contact Us</a>
@@ -38,19 +38,5 @@ defaultAside = toElements(`
 <a target="_out" href="${swtGitHub}/issues">Report a Problem</a>
 <a target="_out" href="${swtGitHub}/blob/master/CONTRIBUTING.md">Contribute</a>
 `);
-
-/*
-$Nav->addNavSeparator("SWT Home", 	"/swt/");
-$Nav->addCustomNav("Widgets", "/swt/widgets/", "_self", 3);
-$Nav->addCustomNav("Snippets", "/swt/snippets/", "_self", 3);
-$Nav->addCustomNav("Examples", "/swt/examples.php", "_self", 3);
-$Nav->addCustomNav("FAQ", "/swt/faq.php", "_self", 3);
-$Nav->addCustomNav("Tools", "/swt/tools.php", "_self", 3);
-$Nav->addCustomNav("Javadoc", "/swt/javadoc.php", "_self", 3);
-$Nav->addCustomNav("Documentation", "/swt/docs.php", "_self", 3);
-$Nav->addCustomNav("Community", "/swt/community.php", "_self", 3);
-$Nav->addCustomNav("Bugs", "/swt/bugs.php", "_self", 3);
-$Nav->addCustomNav("Contact Us", "/swt/#contact", "_self", 3);
-*/
 
 defaultNav = toElements('');
