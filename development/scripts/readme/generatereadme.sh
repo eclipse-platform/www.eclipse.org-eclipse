e92e90dcfbe3adcb53a220a63b0117193b7d026d
Sougandh S <Sougandh.S@ibm.com>
2025-05-27 10:49:16 +0530

    Read Me for Eclipse 4.36 Release (#357)
    This commit includes read me changes & shell script changes for eclipse
    4.36 release.
    
    Fixes : https://github.com/eclipse-platform/www.eclipse.org-eclipse/issues/356

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.34/4\.35/g'  readme_eclipse.html
-sed -i -e 's/4_34/4_35/g' readme_eclipse.html
-sed -i -e 's/4\.34/4\.35/g' readme_eclipse.html
+sed -i -e 's/4\.35/4\.36/g'  readme_eclipse.html
+sed -i -e 's/4_35/4_36/g' readme_eclipse.html
+sed -i -e 's/4\.35/4\.36/g' readme_eclipse.html
 
