4afd2d43c3af017c29a52ff512ab695472808ae1
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-06-01 11:32:10 +0200

    Generalize applyTemplate.sh script
    Take new acknowledgements file into account and simplify/unify the
    script in general.
    
    Follow-up on
    - https://github.com/eclipse-platform/www.eclipse.org-eclipse/pull/361

--- news/scripts/applyTemplate.sh
+++ news/scripts/applyTemplate.sh
@@ -1,5 +1,7 @@
+#!/bin/sh -e
+
 #******************************************************************************
-# Copyright (c) 2019 IBM Corporation and others.
+# Copyright (c) 2019, 2025 IBM Corporation and others.
 #
 # This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License 2.0
@@ -12,40 +14,34 @@
 #     IBM Corporation - initial API and implementation
 #******************************************************************************
 
-#!/bin/sh
-
 if [ "x$1" == "x"  ] || [ "x$2" == "x"  ]; then
  echo "Usage error: Pass release version and simrel release name as arguments"
  echo "For example: ./applyTemplate.sh 4.13 2019-09"
  exit 1
 fi
+eclipseVersion=$1
+simRelVersion=$2
 
-cd ..
-echo "In parent directory of scripts `pwd`"
+cd $(dirname $(readlink -f "$0"))/../../news
+echo "In news directory: $(pwd)"
 
-echo "Checking if directory $1 already exists"
-if [ -d $1 ] 
-then
-   echo "Error: Directory $1 already exists. Exiting..."
+echo "Checking if directory ${eclipseVersion} already exists"
+if [ -d ${eclipseVersion} ]; then
+   echo "Error: Directory ${eclipseVersion} already exists. Exiting..."
    exit 1
 fi
 
-echo "Applying template for $1"
-cp -R '4.x-template' $1
+echo "Applying template for ${eclipseVersion}"
+cp -R '4.x-template' ${eclipseVersion}
 
-echo "Created directory and entering $1"
-cd $1
+echo "Created directory and entering ${eclipseVersion}"
+cd ${eclipseVersion}
 
-echo "Replacing 4.x with $1 to files in `pwd`"
+echo "Replacing 4.x with ${eclipseVersion} and YYYY-MM with ${simRelVersion} to files in $(pwd)"
 
-sed -i'.bak' -e "s/4\.x/$1/g" index.html
-sed -i'.bak' -e "s/4\.x/$1/g" jdt.html
-sed -i'.bak' -e "s/4\.x/$1/g" pde.html
-sed -i'.bak' -e "s/4\.x/$1/g" platform.html
-sed -i'.bak' -e "s/4\.x/$1/g" platform_isv.html
-
-echo "Replacing YYYY-MM with $2 in index.html"
-sed -i'.bak' -e "s/YYYY-MM/$2/g" index.html
-
-echo "Deleting backup files"
-rm *.html.bak
+for file in *.md; do
+  if [ -f "$file" ]; then
+    sed -i --expression "s|4\.x|${eclipseVersion}|g" $file
+    sed -i --expression "s|YYYY-MM|${simRelVersion}|g" $file
+  fi
+done
