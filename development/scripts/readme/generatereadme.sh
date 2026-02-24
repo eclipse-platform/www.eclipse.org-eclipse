ca2f7c9871cdd4db1887032debd2e467a2a2f8f0
sougandhs <sougandh.s@ibm.com>
2026-02-24 18:32:04 +0530

    ReadMe for 4.39
    see https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/3669

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.37/4\.38/g'  readme_eclipse.html
-sed -i -e 's/4_37/4_38/g' readme_eclipse.html
-sed -i -e 's/4\.37/4\.38/g' readme_eclipse.html
+sed -i -e 's/4\.38/4\.39/g'  readme_eclipse.html
+sed -i -e 's/4_38/4_39/g' readme_eclipse.html
+sed -i -e 's/4\.38/4\.39/g' readme_eclipse.html
 
