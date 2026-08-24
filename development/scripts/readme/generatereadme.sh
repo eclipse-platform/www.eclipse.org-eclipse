0a502a97830a890f8e5b17c3bbc8015584e1772b
Sougandh S <sougandh.s@ibm.com>
2026-08-24 10:05:44 +0530

    ReadMe changes for 4.41 Release

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.39/4\.40/g'  readme_eclipse.html
-sed -i -e 's/4_39/4_40/g' readme_eclipse.html
-sed -i -e 's/4\.39/4\.40/g' readme_eclipse.html
+sed -i -e 's/4\.40/4\.41/g'  readme_eclipse.html
+sed -i -e 's/4_40/4_41/g' readme_eclipse.html
+sed -i -e 's/4\.40/4\.41/g' readme_eclipse.html
 
